9b3a7f1dd5609abe8120bce88a0124878c47226f
Leif Jurvetson <leijurv@gmail.com>
2016-01-19 16:42:52 -0800

    updated

--- mcp918/src/minecraft/Start.java
+++ mcp918/src/minecraft/Start.java
@@ -1,18 +1,16 @@
-import java.io.File;
-import java.lang.reflect.Field;
-import java.util.Arrays;
 
+import java.util.Arrays;
 import net.minecraft.client.main.Main;
 
-public class Start
-{
-    public static void main(String[] args)
-    {
-        Main.main(concat(new String[] {"--version", "mcp", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
+public class Start {
+    public static void main(String[] args) {
+        System.out.println(Arrays.asList(args));
+        System.out.println(System.getProperty("java.library.path"));
+        //System.setProperty("java.library.path", System.getProperty("user.home") + "/Dropbox/MineBot/mcp918/jars/versions/1.8.8/1.8.8-natives/");
+        //System.out.println(System.getProperty("java.library.path"));
+        Main.main(concat(new String[]{"--version", "mcp", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
     }
-
-    public static <T> T[] concat(T[] first, T[] second)
-    {
+    public static <T> T[] concat(T[] first, T[] second) {
         T[] result = Arrays.copyOf(first, first.length + second.length);
         System.arraycopy(second, 0, result, first.length, second.length);
         return result;
---
+++ runmc.sh
@@ -0,0 +1,2 @@
+cd mcp918/jars
+java -Djava.library.path=versions/1.8.8/1.8.8-natives/ -jar ../dist/MineBot.jar
