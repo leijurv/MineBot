e563119dbb731caead657f0aa4348d5936c088fc
Leif Jurvetson <leijurv@gmail.com>
2016-01-23 14:31:20 -0800

    lots of comments and cleanup

--- mcp918/src/minebot/minebot/MineBot.java
+++ mcp918/src/minebot/minebot/MineBot.java
@@ -31,8 +31,6 @@ import net.minecraft.world.World;
  * @author leijurv
  */
 public class MineBot {
-    static boolean looking = false;
-    static boolean lookingPitch = false;
     static boolean isThereAnythingInProgress = false;
     public static void main(String[] args) throws IOException, InterruptedException {
         String s = Autorun.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(5) + "../../autorun/runmc.command";
@@ -56,7 +54,6 @@ public class MineBot {
      * Called by minecraft.java
      */
     public static void onTick1() {
-        /////////*System.out.println(isLeftClick + "," + pressTime);*/
         if (Minecraft.theMinecraft.theWorld == null || Minecraft.theMinecraft.thePlayer == null) {
             return;
         }
@@ -71,13 +68,12 @@ public class MineBot {
                 pressTime = -10;
             }
         }
-        looking = false;
+        lookingYaw = false;
         lookingPitch = false;
         EntityPlayerSP thePlayer = Minecraft.theMinecraft.thePlayer;
         World theWorld = Minecraft.theMinecraft.theWorld;
         BlockPos playerFeet = new BlockPos(thePlayer.posX, thePlayer.posY, thePlayer.posZ);
         if (currentPath != null) {
-            //System.out.println("On a path");
             if (currentPath.tick()) {
                 if (currentPath != null && currentPath.failed) {
                     clearPath();
@@ -134,45 +130,36 @@ public class MineBot {
             System.out.println("Jumping because in water and pathfinding");
             jumping = true;
         }
-        if (looking) {
+        if (lookingYaw) {
             System.out.println("desired yaw: " + desiredYaw);
             desiredYaw += 360;
             desiredYaw %= 360;
             float yawDistance = Minecraft.theMinecraft.thePlayer.rotationYaw - desiredYaw;
-            //System.out.println();
-            //System.out.println();
-            //System.out.println(yawDistance);
             if (yawDistance > 180) {
-                yawDistance = yawDistance - 360;
+                yawDistance -= 360;
             } else if (yawDistance < -180) {
-                yawDistance = yawDistance + 360;
+                yawDistance += 360;
             }
-            //System.out.println(yawDistance);
-            if (Math.abs(yawDistance) > (360 / 20)) {
-                yawDistance = Math.signum(yawDistance) * 360 / 20;
+            if (Math.abs(yawDistance) > MAX_YAW_CHANGE_PER_TICK) {
+                yawDistance = Math.signum(yawDistance) * MAX_YAW_CHANGE_PER_TICK;
             }
-            //System.out.println(yawDistance);
             Minecraft.theMinecraft.thePlayer.rotationYaw -= yawDistance;
         }
         if (lookingPitch) {
-            //System.out.println();
             float pitchDistance = Minecraft.theMinecraft.thePlayer.rotationPitch - desiredPitch;
-            //System.out.println(pitchDistance);
             if (pitchDistance > 180) {
-                pitchDistance = pitchDistance - 360;
+                pitchDistance -= 360;
             } else if (pitchDistance < -180) {
-                pitchDistance = pitchDistance + 360;
+                pitchDistance += 360;
             }
-            //System.out.println(pitchDistance);
-            if (Math.abs(pitchDistance) > (360 / 20)) {
-                pitchDistance = Math.signum(pitchDistance) * 360 / 20;
+            if (Math.abs(pitchDistance) > MAX_PITCH_CHANGE_PER_TICK) {
+                pitchDistance = Math.signum(pitchDistance) * MAX_PITCH_CHANGE_PER_TICK;
             }
-            //System.out.println(pitchDistance);
             Minecraft.theMinecraft.thePlayer.rotationPitch -= pitchDistance;
-            //System.out.println();
-            //System.out.println();
         }
     }
+    static final float MAX_YAW_CHANGE_PER_TICK = 360 / 20;
+    static final float MAX_PITCH_CHANGE_PER_TICK = 360 / 20;
     public static boolean wasScreen = false;
     public static boolean calculatingNext = false;
     public static Path currentPath = null;
@@ -214,6 +201,10 @@ public class MineBot {
         pressTime = 0;
         isLeftClick = false;
     }
+    /**
+     * Clears movement, but nothing else. Includes jumping and sneaking, but not
+     * left clicking.
+     */
     public static void clearMovement() {
         jumping = false;
         forward = false;
@@ -222,6 +213,9 @@ public class MineBot {
         backward = false;
         sneak = false;
     }
+    /**
+     * Clears movement, clears the current path, and lets go of left click
+     */
     public static void clearPath() {
         currentPath = null;
         letGoOfLeftClick();
@@ -230,8 +224,8 @@ public class MineBot {
     /**
      * Called by GuiScreen.java
      *
-     * @param message
-     * @return
+     * @param message the message that was sent in chat to trigger this
+     * @return what message should actually be sent. can be null to send nothing
      */
     public static String therewasachatmessage(String message) {
         Minecraft mc = Minecraft.theMinecraft;
@@ -249,6 +243,7 @@ public class MineBot {
         }
         if (text.equals("cancel")) {
             cancelPath();
+            return isThereAnythingInProgress ? "Cancelled it, but btw I'm pathfinding right now" : "Cancelled it";
         }
         if (text.equals("st")) {
             System.out.println(theWorld.getBlockState(playerFeet).getBlock());
@@ -264,16 +259,20 @@ public class MineBot {
             String next = null;
             Scanner t = new Scanner(text.substring("setgoal".length()).trim());
             int[] coords = new int[3];
-            int c = 0;
-            while (t.hasNext() && c < 3) {
-                coords[c++] = t.nextInt();
+            int numCoords = 0;
+            while (t.hasNext() && numCoords < coords.length) {
+                coords[numCoords] = t.nextInt();
+                numCoords++;
             }
-            switch (c) {
+            switch (numCoords) {
                 case 3:
                     goal = new GoalBlock(new BlockPos(coords[0], coords[1], coords[2]));
                     break;
                 case 2:
                     goal = new GoalXZ(coords[0], coords[1]);
+                    break;
+                case 1:
+                    goal = new GoalYLevel(coords[0]);
                     break;
                 default:
                     goal = new GoalBlock(playerFeet);
@@ -281,12 +280,7 @@ public class MineBot {
             }
             return "Set goal to " + goal;
         }
-        if (text.startsWith("goaly")) {
-            goal = new GoalYLevel(Integer.parseInt(text.substring("goaly".length()).trim()));
-            return "Set goal to " + goal;
-        }
         if (text.startsWith("path")) {
-            //boolean stone = message.contains("stone");
             findPathInNewThread(playerFeet);
             return null;
         }
@@ -305,12 +299,19 @@ public class MineBot {
         }
         return message;
     }
-    public static String cancelPath() {
+    /**
+     * Cancel the path
+     *
+     */
+    public static void cancelPath() {
         nextPath = null;
         clearPath();
         Miner.stopMining();
-        return "unset";
     }
+    /**
+     * Go mining in a new thread. Literally starts a new thread that calls
+     * Miner.goMining()
+     */
     public static void goMiningInNewThread() {
         new Thread() {
             @Override
@@ -319,6 +320,13 @@ public class MineBot {
             }
         }.start();
     }
+    /**
+     * Go to the specified Y coordinate. Methods blocks via Thread.sleep until
+     * currentPath is null. Note: does NOT actually check Y coordinate at the
+     * end (so it could have been canceled)
+     *
+     * @param y
+     */
     public static void getToY(int y) {
         if (currentPath != null) {
             cancelPath();
@@ -338,7 +346,12 @@ public class MineBot {
             Logger.getLogger(MineBot.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    public static void findPathInNewThread(BlockPos playerFeet) {
+    /**
+     * In a new thread, pathfind to target blockpos
+     *
+     * @param target
+     */
+    public static void findPathInNewThread(BlockPos target) {
         new Thread() {
             @Override
             public void run() {
@@ -346,19 +359,24 @@ public class MineBot {
                     return;
                 }
                 isThereAnythingInProgress = true;
-                GuiScreen.sendChatMessage("Starting to search for path from " + playerFeet + " to " + goal, true);
-                currentPath = findPath(playerFeet);
+                GuiScreen.sendChatMessage("Starting to search for path from " + target + " to " + goal, true);
+                currentPath = findPath(target);
                 if (!currentPath.goal.isInGoal(currentPath.end)) {
                     GuiScreen.sendChatMessage("I couldn't find that path, but I'm going to get as close as I can", true);
                     isThereAnythingInProgress = false;
                     planAhead();
                 } else {
-                    GuiScreen.sendChatMessage("Finished finding a path from " + playerFeet + " to " + goal, true);
+                    GuiScreen.sendChatMessage("Finished finding a path from " + target + " to " + goal, true);
                     isThereAnythingInProgress = false;
                 }
             }
         }.start();
     }
+    /**
+     * In a new thread, pathfind from currentPath.end to goal. Store resulting
+     * path in nextPath (or in currentPath if calculatingNext was set to false
+     * in the meantime).
+     */
     public static void planAhead() {
         new Thread() {
             @Override
@@ -382,10 +400,16 @@ public class MineBot {
             }
         }.start();
     }
-    public static Path findPath(BlockPos playerFeet) {
-        PathFinder pf = new PathFinder(playerFeet, goal);
+    /**
+     * Actually do the pathfinding
+     *
+     * @param start
+     * @return
+     */
+    public static Path findPath(BlockPos start) {
+        PathFinder pf = new PathFinder(start, goal);
         Path path = pf.calculatePath();
-        GuiScreen.sendChatMessage("calculated " + playerFeet + " to " + path.end, true);
+        GuiScreen.sendChatMessage("calculated " + start + " to " + path.end, true);
         return path;
         /* if (stone) {
          path.showPathInStone();
@@ -412,14 +436,37 @@ public class MineBot {
         }
         return null;
     }
+    /**
+     * The desired yaw, as set by whatever action is happening. Remember to also
+     * set lookingYaw to true if you really want the yaw to change
+     *
+     */
     static float desiredYaw;
+    /**
+     * The desired pitch, as set by whatever action is happening. Remember to
+     * also set lookingPitch to true if you really want the yaw to change
+     *
+     */
     static float desiredPitch;
     /**
-     * Called by our code
+     * Set to true if the action wants the player's yaw to be moved towards
+     * desiredYaw
+     */
+    static boolean lookingYaw = false;
+    /**
+     * Set to true if the action wants the player's pitch to be moved towards
+     * desiredPitch
+     */
+    static boolean lookingPitch = false;
+    /**
+     * Called by our code in order to look in the direction of the center of a
+     * block
      *
-     * @param p
-     * @param alsoDoPitch
-     * @return lookAtCoords(x, y, z, alsoDoPitch)
+     * @param p the position to look at
+     * @param alsoDoPitch whether to set desired pitch or just yaw
+     * @return is the actual player yaw (and actual player pitch, if alsoDoPitch
+     * is true) within ANGLE_THRESHOLD (currently 7°) of looking straight at
+     * this block?
      */
     public static boolean lookAtBlock(BlockPos p, boolean alsoDoPitch) {
         Block b = Minecraft.theMinecraft.theWorld.getBlockState(p).getBlock();
@@ -441,7 +488,21 @@ public class MineBot {
         //System.out.println("Trying to look at " + p + " actually looking at " + whatAreYouLookingAt() + " xyz is " + x + "," + y + "," + z);
         return lookAtCoords(x, y, z, alsoDoPitch);
     }
+    /**
+     * The threshold for how close it tries to get to looking straight at things
+     */
     public static final float ANGLE_THRESHOLD = 7;
+    /**
+     * Look at coordinates
+     *
+     * @param x
+     * @param y
+     * @param z
+     * @param alsoDoPitch also adjust the pitch? if false, y is ignored
+     * @return is the actual player yaw (and actual player pitch, if alsoDoPitch
+     * is true) within ANGLE_THRESHOLD (currently 7°) of looking straight at
+     * these coordinates?
+     */
     public static boolean lookAtCoords(double x, double y, double z, boolean alsoDoPitch) {
         EntityPlayerSP thePlayer = Minecraft.theMinecraft.thePlayer;
         double yDiff = (thePlayer.posY + 1.62) - y;
@@ -449,7 +510,7 @@ public class MineBot {
         double dist = Math.sqrt((thePlayer.posX - x) * (thePlayer.posX - x) + (-thePlayer.posZ + z) * (-thePlayer.posZ + z));
         double pitch = Math.atan2(yDiff, dist);
         desiredYaw = (float) (yaw * 180 / Math.PI);
-        looking = true;
+        lookingYaw = true;
         float yawDist = Math.abs(desiredYaw - thePlayer.rotationYaw);
         boolean withinRange = yawDist < ANGLE_THRESHOLD || yawDist > 360 - ANGLE_THRESHOLD;
         if (alsoDoPitch) {
@@ -460,6 +521,12 @@ public class MineBot {
         }
         return withinRange;
     }
+    /**
+     * calls moveTowardsCoords on the center of this block
+     *
+     * @param p
+     * @return am I moving, or am I still rotating
+     */
     public static boolean moveTowardsBlock(BlockPos p) {
         Block b = Minecraft.theMinecraft.theWorld.getBlockState(p).getBlock();
         double xDiff = (b.getBlockBoundsMinX() + b.getBlockBoundsMaxX()) / 2;
@@ -480,6 +547,18 @@ public class MineBot {
         //System.out.println("Trying to look at " + p + " actually looking at " + whatAreYouLookingAt() + " xyz is " + x + "," + y + "," + z);
         return moveTowardsCoords(x, y, z);
     }
+    /**
+     * Move towards coordinates, not necesarily forwards. e.g. if coordinates
+     * are closest to being directly behind us, go backwards. This minimizes
+     * time spent waiting for rotating
+     *
+     * @param x
+     * @param y
+     * @param z
+     * @return true if we are moving, false if we are still rotating. we will
+     * rotate until within ANGLE_THRESHOLD (currently 7°) of moving in correct
+     * direction
+     */
     public static boolean moveTowardsCoords(double x, double y, double z) {
         EntityPlayerSP thePlayer = Minecraft.theMinecraft.thePlayer;
         float currentYaw = thePlayer.rotationYaw;
@@ -499,7 +578,7 @@ public class MineBot {
         desiredYaw = yaw - tmp;
         System.out.println(currentYaw + " " + yaw + " " + diff + " " + tmp + " " + desiredYaw);
         System.out.println(distanceToForward + " " + distanceToLeft + " " + distanceToRight + " " + distanceToBackward);
-        looking = true;
+        lookingYaw = true;
         if (distanceToForward < ANGLE_THRESHOLD || distanceToForward > 360 - ANGLE_THRESHOLD) {
             forward = true;
             return true;
@@ -521,7 +600,7 @@ public class MineBot {
     /**
      * What block is the player looking at
      *
-     * @return
+     * @return the position of it
      */
     public static BlockPos whatAreYouLookingAt() {
         Minecraft mc = Minecraft.theMinecraft;
