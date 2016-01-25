02f1f4a55c7104d221b3850232f7d923133b96b2
Leif Jurvetson <leijurv@gmail.com>
2016-01-25 12:11:07 -0800

    updated

--- mcp918/src/minebot/minebot/pathfinding/PathRenderer.java
+++ mcp918/src/minebot/minebot/pathfinding/PathRenderer.java
@@ -17,26 +17,23 @@ import org.lwjgl.opengl.GL11;
  * @author galdara
  */
 public class PathRenderer {
-
     public PathRenderer() {
     }
-    
     public static void drawPath() {
-        if(MineBot.currentPath != null) {
-            ArrayList<BlockPos> toDraw=MineBot.currentPath.path;
-                    
-        double doubleX = Minecraft.theMinecraft.thePlayer.posX - 0.5;
-        double doubleY = Minecraft.theMinecraft.thePlayer.posY + 0.1;
-        double doubleZ = Minecraft.theMinecraft.thePlayer.posZ - 0.5;
-        
-        GlStateManager.pushMatrix();
-        GlStateManager.translate(-doubleX, -doubleY, -doubleZ);
-        GlStateManager.color(0.937f, 0.016f, 0.424f);
-        GL11.glBegin(GL11.GL_LINES);
-        GL11.glVertex3f((float) doubleX,(float) doubleY,(float) doubleZ);
-        GL11.glVertex3f((float) doubleX + 1,(float) doubleY,(float) doubleZ);
-        GL11.glEnd();
-        GlStateManager.popMatrix();
+        System.out.println("yolo swag 420 noscope");
+        if (MineBot.currentPath != null) {
+            ArrayList<BlockPos> toDraw = MineBot.currentPath.path;
+            double doubleX = Minecraft.theMinecraft.thePlayer.posX - 0.5;
+            double doubleY = Minecraft.theMinecraft.thePlayer.posY + 0.1;
+            double doubleZ = Minecraft.theMinecraft.thePlayer.posZ - 0.5;
+            GlStateManager.pushMatrix();
+            //lStateManager.translate(-doubleX, -doubleY, -doubleZ);
+            GlStateManager.color(0.937f, 0.016f, 0.424f);
+            GL11.glBegin(GL11.GL_LINES);
+            GL11.glVertex3f((float) doubleX, (float) doubleY, (float) doubleZ);
+            GL11.glVertex3f((float) doubleX + 10, (float) doubleY, (float) doubleZ);
+            GL11.glEnd();
+            GlStateManager.popMatrix();
         }
     }
-}   
+}
