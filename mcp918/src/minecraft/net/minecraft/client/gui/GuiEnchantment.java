0ac955cb3be353289ab3838ab981ae0cd9651402
Leif Jurvetson <leijurv@gmail.com>
2016-01-28 14:48:30 -0800

    updated

--- mcp918/src/minecraft/net/minecraft/client/gui/GuiEnchantment.java
+++ mcp918/src/minecraft/net/minecraft/client/gui/GuiEnchantment.java
@@ -22,25 +22,28 @@ import net.minecraft.world.IWorldNameable;
 import net.minecraft.world.World;
 import org.lwjgl.util.glu.Project;
 
-public class GuiEnchantment extends GuiContainer
-{
-    /** The ResourceLocation containing the Enchantment GUI texture location */
-    private static final ResourceLocation ENCHANTMENT_TABLE_GUI_TEXTURE = new ResourceLocation("textures/gui/container/enchanting_table.png");
-
+public class GuiEnchantment extends GuiContainer {
     /**
-     * The ResourceLocation containing the texture for the Book rendered above the enchantment table
+     * The ResourceLocation containing the Enchantment GUI texture location
+     */
+    private static final ResourceLocation ENCHANTMENT_TABLE_GUI_TEXTURE = new ResourceLocation("textures/gui/container/enchanting_table.png");
+    /**
+     * The ResourceLocation containing the texture for the Book rendered above
+     * the enchantment table
      */
     private static final ResourceLocation ENCHANTMENT_TABLE_BOOK_TEXTURE = new ResourceLocation("textures/entity/enchanting_table_book.png");
-
     /**
-     * The ModelBook instance used for rendering the book on the Enchantment table
+     * The ModelBook instance used for rendering the book on the Enchantment
+     * table
      */
     private static final ModelBook MODEL_BOOK = new ModelBook();
-
-    /** The player inventory currently bound to this GuiEnchantment instance. */
+    /**
+     * The player inventory currently bound to this GuiEnchantment instance.
+     */
     private final InventoryPlayer playerInventory;
-
-    /** A Random instance for use with the enchantment gui */
+    /**
+     * A Random instance for use with the enchantment gui
+     */
     private Random random = new Random();
     private ContainerEnchantment container;
     public int field_147073_u;
@@ -52,59 +55,46 @@ public class GuiEnchantment extends GuiContainer
     public float field_147076_A;
     ItemStack field_147077_B;
     private final IWorldNameable field_175380_I;
-
-    public GuiEnchantment(InventoryPlayer inventory, World worldIn, IWorldNameable p_i45502_3_)
-    {
+    public GuiEnchantment(InventoryPlayer inventory, World worldIn, IWorldNameable p_i45502_3_) {
         super(new ContainerEnchantment(inventory, worldIn));
         this.playerInventory = inventory;
-        this.container = (ContainerEnchantment)this.inventorySlots;
+        this.container = (ContainerEnchantment) this.inventorySlots;
         this.field_175380_I = p_i45502_3_;
     }
-
     /**
-     * Draw the foreground layer for the GuiContainer (everything in front of the items). Args : mouseX, mouseY
+     * Draw the foreground layer for the GuiContainer (everything in front of
+     * the items). Args : mouseX, mouseY
      */
-    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
-    {
+    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
         this.fontRendererObj.drawString(this.field_175380_I.getDisplayName().getUnformattedText(), 12, 5, 4210752);
         this.fontRendererObj.drawString(this.playerInventory.getDisplayName().getUnformattedText(), 8, this.ySize - 96 + 2, 4210752);
     }
-
     /**
      * Called from the main game loop to update the screen.
      */
-    public void updateScreen()
-    {
+    public void updateScreen() {
         super.updateScreen();
         this.func_147068_g();
     }
-
     /**
      * Called when the mouse is clicked. Args : mouseX, mouseY, clickedButton
      */
-    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException
-    {
+    public void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
         super.mouseClicked(mouseX, mouseY, mouseButton);
         int i = (this.width - this.xSize) / 2;
         int j = (this.height - this.ySize) / 2;
-
-        for (int k = 0; k < 3; ++k)
-        {
+        for (int k = 0; k < 3; ++k) {
             int l = mouseX - (i + 60);
             int i1 = mouseY - (j + 14 + 19 * k);
-
-            if (l >= 0 && i1 >= 0 && l < 108 && i1 < 19 && this.container.enchantItem(this.mc.thePlayer, k))
-            {
+            if (l >= 0 && i1 >= 0 && l < 108 && i1 < 19 && this.container.enchantItem(this.mc.thePlayer, k)) {
                 this.mc.playerController.sendEnchantPacket(this.container.windowId, k);
             }
         }
     }
-
     /**
      * Args : renderPartialTicks, mouseX, mouseY
      */
-    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY)
-    {
+    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
         this.mc.getTextureManager().bindTexture(ENCHANTMENT_TABLE_GUI_TEXTURE);
         int i = (this.width - this.xSize) / 2;
@@ -135,31 +125,22 @@ public class GuiEnchantment extends GuiContainer
         GlStateManager.rotate(180.0F, 1.0F, 0.0F, 0.0F);
         float f3 = this.field_147069_w + (this.field_147071_v - this.field_147069_w) * partialTicks + 0.25F;
         float f4 = this.field_147069_w + (this.field_147071_v - this.field_147069_w) * partialTicks + 0.75F;
-        f3 = (f3 - (float)MathHelper.truncateDoubleToInt((double)f3)) * 1.6F - 0.3F;
-        f4 = (f4 - (float)MathHelper.truncateDoubleToInt((double)f4)) * 1.6F - 0.3F;
-
-        if (f3 < 0.0F)
-        {
+        f3 = (f3 - (float) MathHelper.truncateDoubleToInt((double) f3)) * 1.6F - 0.3F;
+        f4 = (f4 - (float) MathHelper.truncateDoubleToInt((double) f4)) * 1.6F - 0.3F;
+        if (f3 < 0.0F) {
             f3 = 0.0F;
         }
-
-        if (f4 < 0.0F)
-        {
+        if (f4 < 0.0F) {
             f4 = 0.0F;
         }
-
-        if (f3 > 1.0F)
-        {
+        if (f3 > 1.0F) {
             f3 = 1.0F;
         }
-
-        if (f4 > 1.0F)
-        {
+        if (f4 > 1.0F) {
             f4 = 1.0F;
         }
-
         GlStateManager.enableRescaleNormal();
-        MODEL_BOOK.render((Entity)null, 0.0F, f3, f4, f2, 0.0F, 0.0625F);
+        MODEL_BOOK.render((Entity) null, 0.0F, f3, f4, f2, 0.0F, 0.0625F);
         GlStateManager.disableRescaleNormal();
         RenderHelper.disableStandardItemLighting();
         GlStateManager.matrixMode(5889);
@@ -169,11 +150,9 @@ public class GuiEnchantment extends GuiContainer
         GlStateManager.popMatrix();
         RenderHelper.disableStandardItemLighting();
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        EnchantmentNameParts.getInstance().reseedRandomGenerator((long)this.container.xpSeed);
+        EnchantmentNameParts.getInstance().reseedRandomGenerator((long) this.container.xpSeed);
         int k = this.container.getLapisAmount();
-
-        for (int l = 0; l < 3; ++l)
-        {
+        for (int l = 0; l < 3; ++l) {
             int i1 = i + 60;
             int j1 = i1 + 20;
             int k1 = 86;
@@ -182,168 +161,109 @@ public class GuiEnchantment extends GuiContainer
             this.mc.getTextureManager().bindTexture(ENCHANTMENT_TABLE_GUI_TEXTURE);
             int l1 = this.container.enchantLevels[l];
             GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-
-            if (l1 == 0)
-            {
+            if (l1 == 0) {
                 this.drawTexturedModalRect(i1, j + 14 + 19 * l, 0, 185, 108, 19);
-            }
-            else
-            {
+            } else {
                 String s1 = "" + l1;
                 FontRenderer fontrenderer = this.mc.standardGalacticFontRenderer;
                 int i2 = 6839882;
-
-                if ((k < l + 1 || this.mc.thePlayer.experienceLevel < l1) && !this.mc.thePlayer.capabilities.isCreativeMode)
-                {
+                if ((k < l + 1 || this.mc.thePlayer.experienceLevel < l1) && !this.mc.thePlayer.capabilities.isCreativeMode) {
                     this.drawTexturedModalRect(i1, j + 14 + 19 * l, 0, 185, 108, 19);
                     this.drawTexturedModalRect(i1 + 1, j + 15 + 19 * l, 16 * l, 239, 16, 16);
                     fontrenderer.drawSplitString(s, j1, j + 16 + 19 * l, k1, (i2 & 16711422) >> 1);
                     i2 = 4226832;
-                }
-                else
-                {
+                } else {
                     int j2 = mouseX - (i + 60);
                     int k2 = mouseY - (j + 14 + 19 * l);
-
-                    if (j2 >= 0 && k2 >= 0 && j2 < 108 && k2 < 19)
-                    {
+                    if (j2 >= 0 && k2 >= 0 && j2 < 108 && k2 < 19) {
                         this.drawTexturedModalRect(i1, j + 14 + 19 * l, 0, 204, 108, 19);
                         i2 = 16777088;
-                    }
-                    else
-                    {
+                    } else {
                         this.drawTexturedModalRect(i1, j + 14 + 19 * l, 0, 166, 108, 19);
                     }
-
                     this.drawTexturedModalRect(i1 + 1, j + 15 + 19 * l, 16 * l, 223, 16, 16);
                     fontrenderer.drawSplitString(s, j1, j + 16 + 19 * l, k1, i2);
                     i2 = 8453920;
                 }
-
                 fontrenderer = this.mc.fontRendererObj;
-                fontrenderer.drawStringWithShadow(s1, (float)(j1 + 86 - fontrenderer.getStringWidth(s1)), (float)(j + 16 + 19 * l + 7), i2);
+                fontrenderer.drawStringWithShadow(s1, (float) (j1 + 86 - fontrenderer.getStringWidth(s1)), (float) (j + 16 + 19 * l + 7), i2);
             }
         }
     }
-
     /**
-     * Draws the screen and all the components in it. Args : mouseX, mouseY, renderPartialTicks
+     * Draws the screen and all the components in it. Args : mouseX, mouseY,
+     * renderPartialTicks
      */
-    public void drawScreen(int mouseX, int mouseY, float partialTicks)
-    {
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         super.drawScreen(mouseX, mouseY, partialTicks);
         boolean flag = this.mc.thePlayer.capabilities.isCreativeMode;
         int i = this.container.getLapisAmount();
-
-        for (int j = 0; j < 3; ++j)
-        {
+        for (int j = 0; j < 3; ++j) {
             int k = this.container.enchantLevels[j];
             int l = this.container.field_178151_h[j];
             int i1 = j + 1;
-
-            if (this.isPointInRegion(60, 14 + 19 * j, 108, 17, mouseX, mouseY) && k > 0 && l >= 0)
-            {
+            if (this.isPointInRegion(60, 14 + 19 * j, 108, 17, mouseX, mouseY) && k > 0 && l >= 0) {
                 List<String> list = Lists.<String>newArrayList();
-
-                if (l >= 0 && Enchantment.getEnchantmentById(l & 255) != null)
-                {
+                if (l >= 0 && Enchantment.getEnchantmentById(l & 255) != null) {
                     String s = Enchantment.getEnchantmentById(l & 255).getTranslatedName((l & 65280) >> 8);
-                    list.add(EnumChatFormatting.WHITE.toString() + EnumChatFormatting.ITALIC.toString() + I18n.format("container.enchant.clue", new Object[] {s}));
+                    list.add(EnumChatFormatting.WHITE.toString() + EnumChatFormatting.ITALIC.toString() + I18n.format("container.enchant.clue", new Object[]{s}));
                 }
-
-                if (!flag)
-                {
-                    if (l >= 0)
-                    {
+                if (!flag) {
+                    if (l >= 0) {
                         list.add("");
                     }
-
-                    if (this.mc.thePlayer.experienceLevel < k)
-                    {
+                    if (this.mc.thePlayer.experienceLevel < k) {
                         list.add(EnumChatFormatting.RED.toString() + "Level Requirement: " + this.container.enchantLevels[j]);
-                    }
-                    else
-                    {
+                    } else {
                         String s1 = "";
-
-                        if (i1 == 1)
-                        {
+                        if (i1 == 1) {
                             s1 = I18n.format("container.enchant.lapis.one", new Object[0]);
+                        } else {
+                            s1 = I18n.format("container.enchant.lapis.many", new Object[]{Integer.valueOf(i1)});
                         }
-                        else
-                        {
-                            s1 = I18n.format("container.enchant.lapis.many", new Object[] {Integer.valueOf(i1)});
-                        }
-
-                        if (i >= i1)
-                        {
+                        if (i >= i1) {
                             list.add(EnumChatFormatting.GRAY.toString() + "" + s1);
-                        }
-                        else
-                        {
+                        } else {
                             list.add(EnumChatFormatting.RED.toString() + "" + s1);
                         }
-
-                        if (i1 == 1)
-                        {
+                        if (i1 == 1) {
                             s1 = I18n.format("container.enchant.level.one", new Object[0]);
+                        } else {
+                            s1 = I18n.format("container.enchant.level.many", new Object[]{Integer.valueOf(i1)});
                         }
-                        else
-                        {
-                            s1 = I18n.format("container.enchant.level.many", new Object[] {Integer.valueOf(i1)});
-                        }
-
                         list.add(EnumChatFormatting.GRAY.toString() + "" + s1);
                     }
                 }
-
                 this.drawHoveringText(list, mouseX, mouseY);
                 break;
             }
         }
     }
-
-    public void func_147068_g()
-    {
+    public void func_147068_g() {
         ItemStack itemstack = this.inventorySlots.getSlot(0).getStack();
-
-        if (!ItemStack.areItemStacksEqual(itemstack, this.field_147077_B))
-        {
+        if (!ItemStack.areItemStacksEqual(itemstack, this.field_147077_B)) {
             this.field_147077_B = itemstack;
-
-            while (true)
-            {
-                this.field_147082_x += (float)(this.random.nextInt(4) - this.random.nextInt(4));
-
-                if (this.field_147071_v > this.field_147082_x + 1.0F || this.field_147071_v < this.field_147082_x - 1.0F)
-                {
+            while (true) {
+                this.field_147082_x += (float) (this.random.nextInt(4) - this.random.nextInt(4));
+                if (this.field_147071_v > this.field_147082_x + 1.0F || this.field_147071_v < this.field_147082_x - 1.0F) {
                     break;
                 }
             }
         }
-
         ++this.field_147073_u;
         this.field_147069_w = this.field_147071_v;
         this.field_147076_A = this.field_147080_z;
         boolean flag = false;
-
-        for (int i = 0; i < 3; ++i)
-        {
-            if (this.container.enchantLevels[i] != 0)
-            {
+        for (int i = 0; i < 3; ++i) {
+            if (this.container.enchantLevels[i] != 0) {
                 flag = true;
             }
         }
-
-        if (flag)
-        {
+        if (flag) {
             this.field_147080_z += 0.2F;
-        }
-        else
-        {
+        } else {
             this.field_147080_z -= 0.2F;
         }
-
         this.field_147080_z = MathHelper.clamp_float(this.field_147080_z, 0.0F, 1.0F);
         float f1 = (this.field_147082_x - this.field_147071_v) * 0.4F;
         float f = 0.2F;
--- mcp918/src/minecraft/net/minecraft/client/gui/GuiRepair.java
+++ mcp918/src/minecraft/net/minecraft/client/gui/GuiRepair.java
@@ -20,26 +20,22 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import org.lwjgl.input.Keyboard;
 
-public class GuiRepair extends GuiContainer implements ICrafting
-{
+public class GuiRepair extends GuiContainer implements ICrafting {
     private static final ResourceLocation anvilResource = new ResourceLocation("textures/gui/container/anvil.png");
     private ContainerRepair anvil;
     private GuiTextField nameField;
     private InventoryPlayer playerInventory;
-
-    public GuiRepair(InventoryPlayer inventoryIn, World worldIn)
-    {
+    public GuiRepair(InventoryPlayer inventoryIn, World worldIn) {
         super(new ContainerRepair(inventoryIn, worldIn, Minecraft.getMinecraft().thePlayer));
         this.playerInventory = inventoryIn;
-        this.anvil = (ContainerRepair)this.inventorySlots;
+        this.anvil = (ContainerRepair) this.inventorySlots;
     }
-
     /**
-     * Adds the buttons (and other controls) to the screen in question. Called when the GUI is displayed and when the
-     * window resizes, the buttonList is cleared beforehand.
+     * Adds the buttons (and other controls) to the screen in question. Called
+     * when the GUI is displayed and when the window resizes, the buttonList is
+     * cleared beforehand.
      */
-    public void initGui()
-    {
+    public void initGui() {
         super.initGui();
         Keyboard.enableRepeatEvents(true);
         int i = (this.width - this.xSize) / 2;
@@ -52,175 +48,132 @@ public class GuiRepair extends GuiContainer implements ICrafting
         this.inventorySlots.removeCraftingFromCrafters(this);
         this.inventorySlots.onCraftGuiOpened(this);
     }
-
     /**
-     * Called when the screen is unloaded. Used to disable keyboard repeat events
+     * Called when the screen is unloaded. Used to disable keyboard repeat
+     * events
      */
-    public void onGuiClosed()
-    {
+    public void onGuiClosed() {
         super.onGuiClosed();
         Keyboard.enableRepeatEvents(false);
         this.inventorySlots.removeCraftingFromCrafters(this);
     }
-
     /**
-     * Draw the foreground layer for the GuiContainer (everything in front of the items). Args : mouseX, mouseY
+     * Draw the foreground layer for the GuiContainer (everything in front of
+     * the items). Args : mouseX, mouseY
      */
-    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
-    {
+    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
         GlStateManager.disableLighting();
         GlStateManager.disableBlend();
         this.fontRendererObj.drawString(I18n.format("container.repair", new Object[0]), 60, 6, 4210752);
-
-        if (this.anvil.maximumCost > 0)
-        {
+        if (this.anvil.maximumCost > 0) {
             int i = 8453920;
             boolean flag = true;
-            String s = I18n.format("container.repair.cost", new Object[] {Integer.valueOf(this.anvil.maximumCost)});
-
-            if (this.anvil.maximumCost >= 40 && !this.mc.thePlayer.capabilities.isCreativeMode)
-            {
+            String s = I18n.format("container.repair.cost", new Object[]{Integer.valueOf(this.anvil.maximumCost)});
+            if (this.anvil.maximumCost >= 40 && !this.mc.thePlayer.capabilities.isCreativeMode) {
                 s = I18n.format("container.repair.expensive", new Object[0]);
                 i = 16736352;
-            }
-            else if (!this.anvil.getSlot(2).getHasStack())
-            {
+            } else if (!this.anvil.getSlot(2).getHasStack()) {
                 flag = false;
-            }
-            else if (!this.anvil.getSlot(2).canTakeStack(this.playerInventory.player))
-            {
+            } else if (!this.anvil.getSlot(2).canTakeStack(this.playerInventory.player)) {
                 i = 16736352;
             }
-
-            if (flag)
-            {
+            if (flag) {
                 int j = -16777216 | (i & 16579836) >> 2 | i & -16777216;
                 int k = this.xSize - 8 - this.fontRendererObj.getStringWidth(s);
                 int l = 67;
-
-                if (this.fontRendererObj.getUnicodeFlag())
-                {
+                if (this.fontRendererObj.getUnicodeFlag()) {
                     drawRect(k - 3, l - 2, this.xSize - 7, l + 10, -16777216);
                     drawRect(k - 2, l - 1, this.xSize - 8, l + 9, -12895429);
-                }
-                else
-                {
+                } else {
                     this.fontRendererObj.drawString(s, k, l + 1, j);
                     this.fontRendererObj.drawString(s, k + 1, l, j);
                     this.fontRendererObj.drawString(s, k + 1, l + 1, j);
                 }
-
                 this.fontRendererObj.drawString(s, k, l, i);
             }
         }
-
         GlStateManager.enableLighting();
     }
-
     /**
-     * Fired when a key is typed (except F11 which toggles full screen). This is the equivalent of
-     * KeyListener.keyTyped(KeyEvent e). Args : character (character on the key), keyCode (lwjgl Keyboard key code)
+     * Fired when a key is typed (except F11 which toggles full screen). This is
+     * the equivalent of KeyListener.keyTyped(KeyEvent e). Args : character
+     * (character on the key), keyCode (lwjgl Keyboard key code)
      */
-    protected void keyTyped(char typedChar, int keyCode) throws IOException
-    {
-        if (this.nameField.textboxKeyTyped(typedChar, keyCode))
-        {
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+        if (this.nameField.textboxKeyTyped(typedChar, keyCode)) {
             this.renameItem();
-        }
-        else
-        {
+        } else {
             super.keyTyped(typedChar, keyCode);
         }
     }
-
-    private void renameItem()
-    {
+    private void renameItem() {
         String s = this.nameField.getText();
         Slot slot = this.anvil.getSlot(0);
-
-        if (slot != null && slot.getHasStack() && !slot.getStack().hasDisplayName() && s.equals(slot.getStack().getDisplayName()))
-        {
+        if (slot != null && slot.getHasStack() && !slot.getStack().hasDisplayName() && s.equals(slot.getStack().getDisplayName())) {
             s = "";
         }
-
         this.anvil.updateItemName(s);
         this.mc.thePlayer.sendQueue.addToSendQueue(new C17PacketCustomPayload("MC|ItemName", (new PacketBuffer(Unpooled.buffer())).writeString(s)));
     }
-
     /**
      * Called when the mouse is clicked. Args : mouseX, mouseY, clickedButton
      */
-    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException
-    {
+    public void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
         super.mouseClicked(mouseX, mouseY, mouseButton);
         this.nameField.mouseClicked(mouseX, mouseY, mouseButton);
     }
-
     /**
-     * Draws the screen and all the components in it. Args : mouseX, mouseY, renderPartialTicks
+     * Draws the screen and all the components in it. Args : mouseX, mouseY,
+     * renderPartialTicks
      */
-    public void drawScreen(int mouseX, int mouseY, float partialTicks)
-    {
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         super.drawScreen(mouseX, mouseY, partialTicks);
         GlStateManager.disableLighting();
         GlStateManager.disableBlend();
         this.nameField.drawTextBox();
     }
-
     /**
      * Args : renderPartialTicks, mouseX, mouseY
      */
-    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY)
-    {
+    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
         this.mc.getTextureManager().bindTexture(anvilResource);
         int i = (this.width - this.xSize) / 2;
         int j = (this.height - this.ySize) / 2;
         this.drawTexturedModalRect(i, j, 0, 0, this.xSize, this.ySize);
         this.drawTexturedModalRect(i + 59, j + 20, 0, this.ySize + (this.anvil.getSlot(0).getHasStack() ? 0 : 16), 110, 16);
-
-        if ((this.anvil.getSlot(0).getHasStack() || this.anvil.getSlot(1).getHasStack()) && !this.anvil.getSlot(2).getHasStack())
-        {
+        if ((this.anvil.getSlot(0).getHasStack() || this.anvil.getSlot(1).getHasStack()) && !this.anvil.getSlot(2).getHasStack()) {
             this.drawTexturedModalRect(i + 99, j + 45, this.xSize, 0, 28, 21);
         }
     }
-
     /**
      * update the crafting window inventory with the items in the list
      */
-    public void updateCraftingInventory(Container containerToSend, List<ItemStack> itemsList)
-    {
+    public void updateCraftingInventory(Container containerToSend, List<ItemStack> itemsList) {
         this.sendSlotContents(containerToSend, 0, containerToSend.getSlot(0).getStack());
     }
-
     /**
-     * Sends the contents of an inventory slot to the client-side Container. This doesn't have to match the actual
-     * contents of that slot. Args: Container, slot number, slot contents
+     * Sends the contents of an inventory slot to the client-side Container.
+     * This doesn't have to match the actual contents of that slot. Args:
+     * Container, slot number, slot contents
      */
-    public void sendSlotContents(Container containerToSend, int slotInd, ItemStack stack)
-    {
-        if (slotInd == 0)
-        {
+    public void sendSlotContents(Container containerToSend, int slotInd, ItemStack stack) {
+        if (slotInd == 0) {
             this.nameField.setText(stack == null ? "" : stack.getDisplayName());
             this.nameField.setEnabled(stack != null);
-
-            if (stack != null)
-            {
+            if (stack != null) {
                 this.renameItem();
             }
         }
     }
-
     /**
-     * Sends two ints to the client-side Container. Used for furnace burning time, smelting progress, brewing progress,
-     * and enchanting level. Normally the first int identifies which variable to update, and the second contains the new
-     * value. Both are truncated to shorts in non-local SMP.
+     * Sends two ints to the client-side Container. Used for furnace burning
+     * time, smelting progress, brewing progress, and enchanting level. Normally
+     * the first int identifies which variable to update, and the second
+     * contains the new value. Both are truncated to shorts in non-local SMP.
      */
-    public void sendProgressBarUpdate(Container containerIn, int varToUpdate, int newValue)
-    {
+    public void sendProgressBarUpdate(Container containerIn, int varToUpdate, int newValue) {
     }
-
-    public void func_175173_a(Container p_175173_1_, IInventory p_175173_2_)
-    {
+    public void func_175173_a(Container p_175173_1_, IInventory p_175173_2_) {
     }
 }
