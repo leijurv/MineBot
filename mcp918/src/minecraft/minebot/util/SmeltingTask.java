f95742853235270ae210299def0ba62c380907a9
Leif Jurvetson <leijurv@gmail.com>
2016-02-02 17:06:04 -0800

    updated

--- mcp918/src/minecraft/minebot/util/SmeltingTask.java
+++ mcp918/src/minecraft/minebot/util/SmeltingTask.java
@@ -5,6 +5,8 @@
  */
 package minebot.util;
 
+import java.util.Map;
+import java.util.Map.Entry;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
 
@@ -13,9 +15,30 @@ import net.minecraft.item.crafting.FurnaceRecipes;
  * @author leijurv
  */
 public class SmeltingTask {
+    private final Map<ItemStack, ItemStack> recipes = getRecipes();
     public SmeltingTask(ItemStack desired) {
+        for (Entry<ItemStack, ItemStack> recipe : recipes.entrySet()) {
+            ItemStack input = recipe.getKey();
+            ItemStack output = recipe.getValue();
+            if (output.getItem().equals(desired.getItem())) {
+                int desiredQuantity = desired.stackSize;
+                int outputQuantity = output.stackSize;
+                int totalQuantity = (int) Math.ceil(((double) desiredQuantity) / ((double) outputQuantity));
+                System.out.println("Recipe from " + input + " to " + output);
+            }
+        }
     }
-    static {
-        FurnaceRecipes.instance().getSmeltingList();
+
+    private static class wrapper {//so that people don't try to directly reference recipess
+        private static Map<ItemStack, ItemStack> recipes = null;
+        public static Map<ItemStack, ItemStack> getRecipes() {
+            if (recipes == null) {
+                recipes = FurnaceRecipes.instance().getSmeltingList();
+            }
+            return recipes;
+        }
+    }
+    public static Map<ItemStack, ItemStack> getRecipes() {
+        return wrapper.getRecipes();
     }
 }
