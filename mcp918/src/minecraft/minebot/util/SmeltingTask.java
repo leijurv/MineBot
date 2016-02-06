435d70a9f4cf95d7286b908580bf0965eec2b97b
Leif Jurvetson <leijurv@gmail.com>
2016-02-05 18:11:03 -0800

    updated

--- mcp918/src/minecraft/minebot/util/SmeltingTask.java
+++ mcp918/src/minecraft/minebot/util/SmeltingTask.java
@@ -108,7 +108,7 @@ public class SmeltingTask {
     int numTicks = -2;//wait a couple extra ticks, for no reason (I guess server lag maybe)
     private void tick() {
         System.out.println(didIPutItInAlreadyPhrasing + " " + isItDone + " " + numTicks + " " + burnTicks);
-        if (furnace != null && didIPutItInAlreadyPhrasing) {
+        if (furnace != null && !didIPutItInAlreadyPhrasing) {
             //we have a furnace, but we haven't put it in yet phrasing
             if (MineBot.couldIReach(furnace)) {
                 MineBot.lookAtBlock(furnace, true);
