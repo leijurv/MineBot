83156be3070e1874d8e7d9585aab457b68befa47
Leif Jurvetson <leijurv@gmail.com>
2016-02-08 10:04:27 -0800

    updated

--- push.sh
+++ push.sh
@@ -1,1 +1,21 @@
-sh push
+#!/bin/bash
+
+MESSAGE=$1
+if [ $# -lt 1 ]; then
+  MESSAGE="updated"
+fi
+if [ $# -gt 1 ]; then
+  echo "Provide exactly one argument. "
+  exit
+fi
+git pull
+find . -name .DS_Store -print0 | xargs -0 rm -f --
+git add --all .
+git commit -m "$MESSAGE"
+git push
+echo "------------"
+echo "Commit ID: "
+git rev-parse HEAD
+echo "-------------"
+time=$(date +"%r")
+echo "Current time: $time"
