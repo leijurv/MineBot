45fd592b1d1646dbd4deb4ada114d7f289ae3c74
Leif Jurvetson <leijurv@gmail.com>
2016-02-09 15:05:09 -0800

    updated

--- mcp918/src/minecraft/minebot/util/CraftingTask.java
+++ mcp918/src/minecraft/minebot/util/CraftingTask.java
@@ -392,7 +392,7 @@ public class CraftingTask {
         }
         GuiScreen.sendChatMessage("Crafting amount " + amount + " of " + currentlyCrafting(), true);
         plan = new ArrayList();
-        tickNumber = 0;
+        tickNumber = -40;
         for (int i = inv ? 9 : 10; i < contain.inventorySlots.inventorySlots.size(); i++) {
             Slot slot = contain.inventorySlots.inventorySlots.get(i);
             if (!slot.getHasStack()) {
