13a2c19ed052d1f93b8a4784a8b62406224ced6a
Leif Jurvetson <leijurv@gmail.com>
2016-02-09 15:17:13 -0800

    updated

--- mcp918/src/minecraft/minebot/util/CraftingTask.java
+++ mcp918/src/minecraft/minebot/util/CraftingTask.java
@@ -377,7 +377,7 @@ public class CraftingTask {
         if (inv) {
             if (Minecraft.theMinecraft.currentScreen == null || !(Minecraft.theMinecraft.currentScreen instanceof GuiInventory)) {
                 System.out.println("Opening");
-                MineBot.openInventory();
+                MineBot.slowOpenInventory();
                 didIOpenMyInventory = true;
             }
         } else {
