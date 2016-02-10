ffa1eb720c4c682789520666591eef52f4b1e857
Leif Jurvetson <leijurv@gmail.com>
2016-02-10 10:22:05 -0800

    updated

--- mcp918/src/minecraft/minebot/util/CraftingTask.java
+++ mcp918/src/minecraft/minebot/util/CraftingTask.java
@@ -570,7 +570,7 @@ public class CraftingTask {
     }
     public static boolean ensureCraftingDesired(Item item, int quantity) {
         CraftingTask craftingTableTask = CraftingTask.findOrCreateCraftingTask(new ItemStack(item, 0));
-        if (craftingTableTask.isDone() && craftingTableTask.alreadyHas >= quantity) {
+        if (craftingTableTask.isDone() && craftingTableTask.alreadyHas >= quantity && craftingTableTask.stackSize > 0) {
             craftingTableTask.decreaseNeededAmount(1);
             return true;
         }
