927dd74ff796be038b0a11b71cbd94827c71f533
Leif Jurvetson <leijurv@gmail.com>
2016-03-23 09:06:12 -0700

    updated

--- mcp918/src/minecraft/minebot/PathRenderer.java
+++ mcp918/src/minecraft/minebot/PathRenderer.java
@@ -46,32 +46,51 @@ public class PathRenderer {
         /*for (BlockPos pos : path.path) {
          drawSelectionBox(player, pos, partialTicks, color);
          }*/
-        for (int i = 0; i < path.path.size() - 1; i++) {
-            drawLine(player, path.path.get(i), path.path.get(i + 1), partialTicks, color);
-        }
-    }
-    public static void drawLine(EntityPlayer player, BlockPos bp1, BlockPos bp2, float partialTicks, Color color) {
         GlStateManager.enableBlend();
         GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
         GlStateManager.color(color.getColorComponents(null)[0], color.getColorComponents(null)[1], color.getColorComponents(null)[2], 0.4F);
         GL11.glLineWidth(3.0F);
         GlStateManager.disableTexture2D();
         GlStateManager.depthMask(false);
+        for (int i = 0; i < path.path.size() - 1; i++) {
+            BlockPos a = path.path.get(i);
+            BlockPos b = path.path.get(i + 1);
+            drawLine(player, a.getX(), a.getY(), a.getZ(), b.getX(), b.getY(), b.getZ(), partialTicks);
+        }
+        //GlStateManager.color(0.0f, 0.0f, 0.0f, 0.4f);
+        for (int i = 0; i < path.path.size() - 2; i++) {
+            BlockPos a = path.path.get(i);
+            BlockPos b = path.path.get(i + 1);
+            BlockPos c = path.path.get(i + 2);
+            if (a.getY() != b.getY() || b.getY() != c.getY()) {
+                continue;
+            }
+            if (b.getX() - a.getX() == c.getX() - b.getX() && b.getZ() - a.getZ() == c.getZ() - b.getZ()) {
+                continue;
+            }
+            double bp1x = (a.getX() + b.getX()) * 0.5D;
+            double bp1z = (a.getZ() + b.getZ()) * 0.5D;
+            double bp2x = (c.getX() + b.getX()) * 0.5D;
+            double bp2z = (c.getZ() + b.getZ()) * 0.5D;
+            drawLine(player, bp1x, a.getY(), bp1z, bp2x, a.getY(), bp2z, partialTicks);
+        }
+        GlStateManager.depthMask(true);
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+    }
+    public static void drawLine(EntityPlayer player, double bp1x, double bp1y, double bp1z, double bp2x, double bp2y, double bp2z, float partialTicks) {
         Tessellator tessellator = Tessellator.getInstance();
         WorldRenderer worldrenderer = tessellator.getWorldRenderer();
         double d0 = player.lastTickPosX + (player.posX - player.lastTickPosX) * (double) partialTicks;
         double d1 = player.lastTickPosY + (player.posY - player.lastTickPosY) * (double) partialTicks;
         double d2 = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * (double) partialTicks;
         worldrenderer.begin(3, DefaultVertexFormats.POSITION);
-        worldrenderer.pos(bp1.getX() + 0.5D - d0, bp1.getY() + 0.5D - d1, bp1.getZ() + 0.5D - d2).endVertex();
-        worldrenderer.pos(bp2.getX() + 0.5D - d0, bp2.getY() + 0.5D - d1, bp2.getZ() + 0.5D - d2).endVertex();
-        worldrenderer.pos(bp2.getX() + 0.5D - d0, bp2.getY() + 0.53D - d1, bp2.getZ() + 0.5D - d2).endVertex();
-        worldrenderer.pos(bp1.getX() + 0.5D - d0, bp1.getY() + 0.53D - d1, bp1.getZ() + 0.5D - d2).endVertex();
-        worldrenderer.pos(bp1.getX() + 0.5D - d0, bp1.getY() + 0.5D - d1, bp1.getZ() + 0.5D - d2).endVertex();
+        worldrenderer.pos(bp1x + 0.5D - d0, bp1y + 0.5D - d1, bp1z + 0.5D - d2).endVertex();
+        worldrenderer.pos(bp2x + 0.5D - d0, bp2y + 0.5D - d1, bp2z + 0.5D - d2).endVertex();
+        worldrenderer.pos(bp2x + 0.5D - d0, bp2y + 0.53D - d1, bp2z + 0.5D - d2).endVertex();
+        worldrenderer.pos(bp1x + 0.5D - d0, bp1y + 0.53D - d1, bp1z + 0.5D - d2).endVertex();
+        worldrenderer.pos(bp1x + 0.5D - d0, bp1y + 0.5D - d1, bp1z + 0.5D - d2).endVertex();
         tessellator.draw();
-        GlStateManager.depthMask(true);
-        GlStateManager.enableTexture2D();
-        GlStateManager.disableBlend();
     }
     public static void drawSelectionBox(EntityPlayer player, BlockPos blockpos, float partialTicks, Color color) {
         GlStateManager.enableBlend();
