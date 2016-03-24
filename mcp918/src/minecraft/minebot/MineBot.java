e851ff0d1c0e0fc277a59cfbf53cc32b8ce9accd
Leif Jurvetson <leijurv@gmail.com>
2016-03-24 10:44:32 -0700

    updated

--- mcp918/src/minecraft/minebot/MineBot.java
+++ mcp918/src/minecraft/minebot/MineBot.java
@@ -26,8 +26,6 @@ import minebot.util.SchematicBuilder;
 import minebot.util.SmeltingTask;
 import minebot.util.ToolSet;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockLadder;
-import net.minecraft.block.BlockVine;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.GuiScreen;
@@ -41,7 +39,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.client.C16PacketClientStatus;
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 import org.lwjgl.input.Keyboard;
@@ -160,7 +157,7 @@ public class MineBot {
             currentBuilder.tick();
         }
         if (parkour) {
-            parkour();
+            Parkour.parkour();
         }
         if (currentPath != null && ManagerTick.tickPath) {
             if (currentPath.tick()) {
@@ -260,46 +257,6 @@ public class MineBot {
         for (Class c : managers) {
             Manager.tick(c, false);
         }
-    }
-    public static void parkour() {
-        EntityPlayerSP thePlayer = Minecraft.theMinecraft.thePlayer;
-        BlockPos playerFeet = thePlayer.getPosition0();
-        BlockPos down = playerFeet.down();
-        BlockPos prev = down.offset(thePlayer.getHorizontalFacing().getOpposite());
-        boolean onAir = Minecraft.theMinecraft.theWorld.getBlockState(down).getBlock().equals(Block.getBlockById(0));
-        boolean jumpAnyway = preemptivejump();
-        if (onAir || jumpAnyway) {
-            if ((thePlayer.isSprinting() && !Minecraft.theMinecraft.theWorld.getBlockState(prev).getBlock().equals(Block.getBlockById(0))) || !Minecraft.theMinecraft.theWorld.getBlockState(playerFeet.offset(thePlayer.getHorizontalFacing())).getBlock().equals(Block.getBlockById(0))) {
-                double distX = Math.abs(thePlayer.posX - (prev.getX() + 0.5D));
-                distX *= Math.abs(prev.getX() - down.getX());
-                double distZ = Math.abs(thePlayer.posZ - (prev.getZ() + 0.5D));
-                distZ *= Math.abs(prev.getZ() - down.getZ());
-                double dist = distX + distZ;
-                thePlayer.rotationYaw = Math.round(thePlayer.rotationYaw / 90) * 90;
-                if (dist > 0.7) {
-                    MovementManager.jumping = true;
-                    Out.gui("Parkour jumping!!!", Out.Mode.Standard);
-                }
-            }
-        }
-    }
-    public static boolean preemptivejump() {
-        EntityPlayerSP thePlayer = Minecraft.theMinecraft.thePlayer;
-        BlockPos playerFeet = thePlayer.getPosition0();
-        EnumFacing dir = thePlayer.getHorizontalFacing();
-        for (int height = 0; height < 3; height++) {
-            BlockPos bp = playerFeet.offset(dir, 1).up(height);
-            if (!Minecraft.theMinecraft.theWorld.getBlockState(bp).getBlock().equals(Block.getBlockById(0))) {
-                return Action.canWalkOn(playerFeet.offset(dir, 1));
-            }
-        }
-        for (int height = 0; height < 3; height++) {
-            BlockPos bp = playerFeet.offset(dir, 2).up(height);
-            if (!Minecraft.theMinecraft.theWorld.getBlockState(bp).getBlock().equals(Block.getBlockById(0))) {
-                return Action.canWalkOn(playerFeet.offset(dir, 2));
-            }
-        }
-        return Action.canWalkOn(playerFeet.offset(dir, 3));
     }
     public static void openInventory() {
         Out.gui("real open", Out.Mode.Debug);
