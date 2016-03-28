3e315039853a58f2ad4ebfeef76dab34ddbc81c1
Leif Jurvetson <leijurv@gmail.com>
2016-03-28 09:55:40 -0700

    updated

--- mcp918/src/minecraft/minebot/pathfinding/actions/ActionWalkDiagonal.java
+++ mcp918/src/minecraft/minebot/pathfinding/actions/ActionWalkDiagonal.java
@@ -22,7 +22,7 @@ public class ActionWalkDiagonal extends ActionPlaceOrBreak {
         //super(start, start.offset(dir1).offset(dir2), new BlockPos[]{start.offset(dir1), start.offset(dir1).up(), start.offset(dir2), start.offset(dir2).up(), start.offset(dir1).offset(dir2), start.offset(dir1).offset(dir2).up()}, new BlockPos[]{start.offset(dir1).offset(dir2).down()});
     }
     public ActionWalkDiagonal(BlockPos start, BlockPos dir1, BlockPos dir2, EnumFacing drr2) {
-        this(start, dir1, dir2, dir1.offset(drr2));
+        this(start, dir1.offset(drr2), dir1, dir2);
     }
     public ActionWalkDiagonal(BlockPos start, BlockPos end, BlockPos dir1, BlockPos dir2) {
         super(start, end, new BlockPos[]{dir1, dir1.up(), dir2, dir2.up(), end, end.up()}, new BlockPos[]{end.down()});
