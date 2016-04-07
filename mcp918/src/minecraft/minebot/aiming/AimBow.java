1f6c8ef50daa0b18c5ec7bd12572d2d08db668df
Leif Jurvetson <leijurv@gmail.com>
2016-04-07 06:51:23 -0700

    updated

--- mcp918/src/minecraft/minebot/aiming/AimBow.java
+++ mcp918/src/minecraft/minebot/aiming/AimBow.java
@@ -7,8 +7,6 @@ package minebot.aiming;
 
 import minebot.ui.LookManager;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockAir;
-import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
@@ -21,6 +19,7 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBow;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.MathHelper;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.Color;
 import org.lwjgl.util.ReadableColor;
@@ -30,19 +29,15 @@ import org.lwjgl.util.ReadableColor;
  * @author galdara
  */
 public class AimBow {
-    
     public static double tick = 0;
-    
     public static boolean canHit(BlockPos target) {
         return false;
     }
-    
     public static void render(EntityPlayer player, float partialTicks) {
-        if(player.getCurrentEquippedItem() != null && player.getCurrentEquippedItem().getItem() instanceof ItemBow) {
+        if (player.getCurrentEquippedItem() != null && player.getCurrentEquippedItem().getItem() instanceof ItemBow) {
             drawArrowArc(player, new Arrow(Constants.BowConstants.bowFullDraw, Helper.degreesToRadians(player.rotationPitch * -1)), Color.BLUE, Color.RED, partialTicks);
         }
     }
-    
     public static void drawArrowArc(EntityPlayer player, Arrow arrow, ReadableColor airColor, ReadableColor liquidColor, float partialTicks) {
         tick++;
         GlStateManager.enableBlend();
@@ -52,26 +47,26 @@ public class AimBow {
         GlStateManager.depthMask(false);
         GlStateManager.color(airColor.getRed(), airColor.getGreen(), airColor.getBlue());
         double previousDist = 0;
-        double previousX = player.posX + previousDist;
-        double previousY = arrow.getVerticalPositionAtHorizontalPosition(previousDist) + player.posY + (double)player.getEyeHeight() - 0.10000000149011612D;;
-        double previousZ = player.posZ + previousDist;
-        for(double dist = Constants.BowConstants.renderTrajectoryIncrement; dist < Constants.BowConstants.renderTrajectoryCutoff; dist += Constants.BowConstants.renderTrajectoryIncrement) {
+        double previousX = player.posX + previousDist - (double) (MathHelper.cos(player.rotationPitch / 180.0F * (float) Math.PI) * 0.16F);
+        double previousY = arrow.getVerticalPositionAtHorizontalPosition(previousDist) + player.posY + (double) player.getEyeHeight() - 0.10000000149011612D;;
+        double previousZ = player.posZ + previousDist - (double) (MathHelper.sin(player.rotationPitch / 180.0F * (float) Math.PI) * 0.16F);;
+        for (double dist = Constants.BowConstants.renderTrajectoryIncrement; dist < Constants.BowConstants.renderTrajectoryCutoff; dist += Constants.BowConstants.renderTrajectoryIncrement) {
             BlockPos blockPos = new BlockPos(previousX, previousY, previousZ);
             IBlockState blockState = Minecraft.theMinecraft.theWorld.getBlockState(blockPos);
             Block block = blockState.getBlock();
-            if(Constants.BlockConstants.materialsPassable.contains(block.getMaterial())) {
-                if(Constants.BlockConstants.materialsLiquid.contains(block.getMaterial()) && arrow.isInAir) {
+            if (Constants.BlockConstants.materialsPassable.contains(block.getMaterial())) {
+                if (Constants.BlockConstants.materialsLiquid.contains(block.getMaterial()) && arrow.isInAir) {
                     arrow.setIsInAir(false);
-                    GlStateManager.color(liquidColor.getRed(), liquidColor.getGreen(), liquidColor.getBlue()); 
-                } else if(!arrow.isInAir) {
+                    GlStateManager.color(liquidColor.getRed(), liquidColor.getGreen(), liquidColor.getBlue());
+                } else if (!arrow.isInAir) {
                     arrow.setIsInAir(true);
                     GlStateManager.color(airColor.getRed(), airColor.getGreen(), airColor.getBlue());
                 }
-                double currentX = (Math.sin(Helper.degreesToRadians(Helper.translateHalfAngle(player.rotationYaw * -1))) * dist) + player.posX;
-                double currentY = arrow.getVerticalPositionAtHorizontalPosition(dist) + player.posY + (double)player.getEyeHeight() - 0.10000000149011612D;
-                double currentZ = (Math.cos(Helper.degreesToRadians(Helper.translateHalfAngle(player.rotationYaw * -1))) * dist) + player.posZ;
+                double currentX = (Math.sin(Helper.degreesToRadians(Helper.translateHalfAngle(player.rotationYaw * -1))) * dist) + player.posX - (double) (MathHelper.cos(player.rotationPitch / 180.0F * (float) Math.PI) * 0.16F);
+                double currentY = arrow.getVerticalPositionAtHorizontalPosition(dist) + player.posY + (double) player.getEyeHeight() - 0.10000000149011612D;
+                double currentZ = (Math.cos(Helper.degreesToRadians(Helper.translateHalfAngle(player.rotationYaw * -1))) * dist) + player.posZ - (double) (MathHelper.sin(player.rotationPitch / 180.0F * (float) Math.PI) * 0.16F);
                 drawLine(player, previousX, previousY, previousZ, currentX, currentY, currentZ, partialTicks);
-                if(tick == 60) {
+                if (tick == 60) {
                     System.out.println("--- NEW TOTAL TICK ---");
                     System.out.println("Current yaw: " + player.rotationYaw);
                     System.out.println("Current position: X" + player.posX + "  #  Y" + player.posY + "  #  Z" + player.posZ);
@@ -91,14 +86,13 @@ public class AimBow {
                 break;
             }
         }
-        if(tick == 60) {
+        if (tick == 60) {
             tick = 0;
         }
         GlStateManager.depthMask(true);
         GlStateManager.enableTexture2D();
         GlStateManager.disableBlend();
     }
-    
     public static void drawLine(EntityPlayer player, double bp1x, double bp1y, double bp1z, double bp2x, double bp2y, double bp2z, float partialTicks) {
         Tessellator tessellator = Tessellator.getInstance();
         WorldRenderer worldrenderer = tessellator.getWorldRenderer();
@@ -106,14 +100,13 @@ public class AimBow {
         double d1 = player.lastTickPosY + (player.posY - player.lastTickPosY) * (double) partialTicks;
         double d2 = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * (double) partialTicks;
         worldrenderer.begin(3, DefaultVertexFormats.POSITION);
-        worldrenderer.pos(bp1x  - d0, bp1y - d1, bp1z - d2).endVertex();
+        worldrenderer.pos(bp1x - d0, bp1y - d1, bp1z - d2).endVertex();
         worldrenderer.pos(bp2x - d0, bp2y - d1, bp2z - d2).endVertex();
         worldrenderer.pos(bp2x - d0, bp2y - d1, bp2z - d2).endVertex();
         worldrenderer.pos(bp1x - d0, bp1y - d1, bp1z - d2).endVertex();
         worldrenderer.pos(bp1x - d0, bp1y - d1, bp1z - d2).endVertex();
         tessellator.draw();
     }
-    
     public static void drawSelectionBox(EntityPlayer player, BlockPos blockpos, float partialTicks, ReadableColor color) {
         GlStateManager.enableBlend();
         GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
@@ -162,10 +155,7 @@ public class AimBow {
         GlStateManager.enableTexture2D();
         GlStateManager.disableBlend();
     }
-    
     public static void aimAtEntity(Entity entity) {
         LookManager.setDesiredYaw((float) Math.atan2(entity.posX - Minecraft.theMinecraft.thePlayer.posX, entity.posZ - Minecraft.theMinecraft.thePlayer.posZ));
     }
-    
 }
-
