ba9466712acec55f1988a701adc351a30f837285
Leif Jurvetson <leijurv@gmail.com>
2016-04-07 08:51:58 -0700

    updated

--- mcp918/src/minecraft/minebot/aiming/AimBow.java
+++ mcp918/src/minecraft/minebot/aiming/AimBow.java
@@ -35,7 +35,7 @@ public class AimBow {
     }
     public static void render(EntityPlayer player, float partialTicks) {
         if (player.getCurrentEquippedItem() != null && player.getCurrentEquippedItem().getItem() instanceof ItemBow) {
-            //drawArrowArc(player, new Arrow(Constants.BowConstants.bowFullDraw, Helper.degreesToRadians(player.rotationPitch * -1)), Color.BLUE, Color.RED, partialTicks);
+            drawArrowArc(player, new Arrow(Constants.BowConstants.bowFullDraw, Helper.degreesToRadians(player.rotationPitch * -1)), Color.GREEN, Color.RED, partialTicks);
             drawArrowArc(player, new Arrow(player, Constants.BowConstants.bowFullDraw), Color.BLUE, Color.RED, partialTicks);
         }
     }
